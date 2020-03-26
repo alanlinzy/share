29c67c64f75417958dee22946564605059789f66
alanlinzy <lzy19970509@qq.com>
2020-03-26 10:02:46 -0400

    shell0

---
+++ shell_code_hw0.asm
@@ -0,0 +1,26 @@
+segment .data
+
+	shellpath db "/bin/sh0"
+
+segment .text
+	global _start
+
+_start:
+
+	xor rax, rax
+        mov rcx,0x3068732f6e69622f
+        push rcx
+        mov rbx,rsp
+	
+
+	mov [rbx+7], al
+	mov [rbx+8], rbx
+	mov [rbx+16], rax
+
+	mov al, 59
+	lea rdi, [rbx]
+	lea rsi, [rbx+8]
+	lea rdx, [rbx+16]
+
+	syscall
+
